81a179913357d74df92d1214c2f234bd5ed00149
valeryyjohnsonn <49496668+valeryyjohnsonn@users.noreply.github.com>
2021-09-06 13:04:48 -0500

    React Properties Challenge

--- src/contacts.js
+++ src/contacts.js
@@ -19,5 +19,14 @@ const contacts = [
       "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
     phone: "+918 372 574",
     email: "gmail@chucknorris.com"
+  },
+  {
+    name: "Allie Long",
+    imgURL:
+      "https://th.bing.com/th/id/OIP.vyVBnUufrio6hWjmhtyZgQHaGY?w=217&h=188&c=7&r=0&o=5&pid=1.7",
+    phone: "+654 789 3120",
+    email: "long@allie.com"
   }
 ];
+
+export default contacts;
